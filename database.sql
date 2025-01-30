1c610da36ac520fb19ce80e83b33557fca5ddf84
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2025-01-30 03:04:19 -0600

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS cargo_deliveries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    player_id VARCHAR(50) NOT NULL,
+    deliveries INT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
