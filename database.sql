c0eb7201a0c1edf73dcd933ff94ed0bae68859d0
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-05-13 12:45:01 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS cargo_deliveries (
     id INT AUTO_INCREMENT PRIMARY KEY,
     player_id VARCHAR(50) NOT NULL,
     deliveries INT NOT NULL,
+    distance FLOAT DEFAULT 0,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
